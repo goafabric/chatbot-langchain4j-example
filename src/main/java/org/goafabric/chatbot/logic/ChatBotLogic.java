9e8caf192e223bb4ae07ad7910fc2c36ff242b66
Andreas Mautsch <amautsch>
2023-11-22 09:10:49 +0100

    changes

--- src/main/java/org/goafabric/chatbot/logic/ChatBotLogic.java
+++ src/main/java/org/goafabric/chatbot/logic/ChatBotLogic.java
@@ -2,21 +2,28 @@ package org.goafabric.chatbot.logic;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.example.ApiKeys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import static java.time.Duration.ofSeconds;
 
 @Component
 public class ChatBotLogic {
     private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
     public void run() {
-        helloWorld();
+        //prompt("Hello world!");
+        prompt("What is the meaning of life ?");
     }
 
-    private  void helloWorld() {
-        ChatLanguageModel model = OpenAiChatModel.withApiKey("demo");
-        String answer = model.generate("Hello world!");
-        log.info(answer);
+    private void prompt(String prompt) {
+        ChatLanguageModel model = OpenAiChatModel.builder().apiKey(ApiKeys.OPENAI_API_KEY)
+                .timeout(ofSeconds(20)).build();
+
+        log.info(prompt);
+        log.info("thinking ...");
+        log.info(model.generate(prompt));
     }
 }

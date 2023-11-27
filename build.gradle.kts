07a02ffd81f42d0ccd08c36cd6c470c76939060d
Andreas Mautsch <amautsch>
2023-11-27 18:15:34 +0100

    refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("dev.langchain4j:langchain4j:0.24.0")
     implementation("dev.langchain4j:langchain4j-open-ai:0.24.0")
     implementation("dev.langchain4j:langchain4j-ollama:0.24.0")
+
     implementation("org.mapdb:mapdb:3.0.9")
     implementation("dev.langchain4j:langchain4j-embeddings-all-minilm-l6-v2:0.24.0")
 

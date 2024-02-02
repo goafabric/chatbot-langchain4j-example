bbebcb51da1c5fda872b62364167c6551c04ad9a
Andreas Mautsch <amautsch>
2024-02-02 14:39:13 +0100

    changes

--- src/test/java/org/goafabric/opennlp/OpenNlpIT.java
+++ src/test/java/org/goafabric/opennlp/OpenNlpIT.java
@@ -11,7 +11,7 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
-public class OpneNLPIt {
+public class OpenNlpIT {
 
     @Test
     public void test() throws IOException {

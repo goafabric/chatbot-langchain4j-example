499a4ed00b0b647b6be302845fa3ede83ff065a7
Andreas Mautsch <amautsch>
2024-02-06 15:51:09 +0100

    soundex added

--- src/main/java/org/goafabric/soundex/repository/PatientRepository.java
+++ src/main/java/org/goafabric/soundex/repository/PatientRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface PatientRepository extends CrudRepository<PatientEo, String> {
+    //@Query("SELECT pe1_0 FROM PatientEo pe1_0 " +
+    //        "WHERE UPPER(pe1_0.familyName) = UPPER(:familyName) OR pe1_0.familySoundex = :familySoundex")
     List<PatientEo> findByFamilyNameEqualsIgnoreCaseOrFamilySoundex(String familyName, String familySoundex);
 }

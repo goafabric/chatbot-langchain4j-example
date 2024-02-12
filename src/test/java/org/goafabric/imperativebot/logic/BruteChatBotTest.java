f60ed84261a0e4b54e9e675b1cf92973255071b2
Andreas Mautsch <amautsch>
2024-02-12 14:47:11 +0100

    changes

--- src/test/java/org/goafabric/imperativebot/logic/BruteChatBotTest.java
+++ src/test/java/org/goafabric/imperativebot/logic/BruteChatBotTest.java
@@ -16,9 +16,9 @@ class BruteChatBotTest {
 
     @Test
     public void createSearchResultNameAndType() {
-        assertThat(chatBot.createSearchResult("I am searching all Diagnosis for Homer").patientName().toString()).isEqualTo("Homer Simpson");
-        assertThat(chatBot.createSearchResult("I am searching all Diagnosis and Anamnesis for Homer").medicalRecordTypes()).contains(MedicalRecordType.CONDITION);
-        assertThat(chatBot.createSearchResult("I am searching all Diagnosis and Anamnesis for Homer").medicalRecordTypes()).contains(MedicalRecordType.ANAMNESIS);
+        assertThat(chatBot.createSearchResult("I am searching all Diagnosis for Monty").patientName().getFullName()).isEqualTo("Monty Burns");
+        assertThat(chatBot.createSearchResult("I am searching all Diagnosis and Anamnesis for Monty").medicalRecordTypes()).contains(MedicalRecordType.CONDITION);
+        assertThat(chatBot.createSearchResult("I am searching all Diagnosis and Anamnesis for Monty").medicalRecordTypes()).contains(MedicalRecordType.ANAMNESIS);
 
         assertThat(chatBot.createSearchResult("").patientName()).isNull();
         assertThat(chatBot.createSearchResult("").medicalRecordTypes()).isEmpty();
@@ -26,9 +26,9 @@ class BruteChatBotTest {
 
     @Test
     public void createSearchResultDisplayText() {
-        assertThat(chatBot.createSearchResult("I am searching for all Diagnosis with text sugar and mice").displayText()).isEqualTo("sugar");
-        assertThat(chatBot.createSearchResult("I am searching for all Diagnosis that contain sugar and mice").displayText()).isEqualTo("sugar");
-        assertThat(chatBot.createSearchResult("I am searching for all Diagnosis that contain text sugar and mice").displayText()).isEqualTo("sugar");
+        assertThat(chatBot.createSearchResult("I am searching for all Diagnosis for Monty with text sugar and mice").displayText()).isEqualTo("sugar");
+        assertThat(chatBot.createSearchResult("I am searching for all Diagnosis for Monty that contain sugar and mice").displayText()).isEqualTo("sugar");
+        assertThat(chatBot.createSearchResult("I am searching for all Diagnosis for Monty that contain text sugar and mice").displayText()).isEqualTo("sugar");
     }
 
     @Test
@@ -36,22 +36,21 @@ class BruteChatBotTest {
         var prevPatientId = "1";
 
         assertThat(chatBot.chat("hi", prevPatientId)).isEmpty();
-        assertThat(chatBot.chat("I am searching for Bart", prevPatientId)).isNotEmpty();
-        assertThat(chatBot.chat("I am searching for all data for Monty", prevPatientId)).isEmpty(); //monty has no data attached
+        assertThat(chatBot.chat("I am searching for Monty", prevPatientId)).isNotEmpty();
+        assertThat(chatBot.chat("I am searching for all Diagnosis for Homer", prevPatientId)).isEmpty(); //Homer has no data attached
 
         assertThat(chatBot.chat("I am searching all Diagnosis and Anamnesis for Bart that contain nothing", prevPatientId)).isEmpty();
         assertThat(chatBot.chat("I am searching all Diagnosis and Anamnesis", prevPatientId)).isNotEmpty();
 
-        var medicalRecords1 = chatBot.chat("I am searching all Diagnosis and Anamnesis for Bart that contain sugar", prevPatientId);
+        var medicalRecords1 = chatBot.chat("I am searching all Diagnosis and Anamnesis for Monty that contain sugar", prevPatientId);
         //medicalRecords1.stream().forEach(m -> System.out.println(m.toString()));
         assertThat(medicalRecords1).hasSize(1);
 
-        assertThat(chatBot.chat("I am searching all Diagnosis and Anamnesis for Bart that contain", prevPatientId)).isNotEmpty();
+        assertThat(chatBot.chat("I am searching all Diagnosis and Anamnesis for Monty that contain", prevPatientId)).isNotEmpty();
 
-        var medicalRecords2 = chatBot.chat("I am searching all Diagnosis and Anamnesis for Simpson", prevPatientId);
-        //medicalRecords2.stream().forEach(m -> System.out.println(m.toString()));
+        var medicalRecords2 = chatBot.chat("I am searching all Diagnosis and Anamnesis for Burns", prevPatientId);
+        medicalRecords2.stream().forEach(m -> System.out.println(m.toString()));
         assertThat(medicalRecords2).hasSize(6);
-
     }
 
 }

27cb5ace626b3c0d44a8ac889a69dd72a9579dfe
Andreas Mautsch <amautsch>
2024-09-18 08:54:36 +0200

    upgrades

--- src/main/java/org/goafabric/dbagent/ai/AIConfiguration.java
+++ src/main/java/org/goafabric/dbagent/ai/AIConfiguration.java
@@ -2,6 +2,7 @@ package org.goafabric.dbagent.ai;
 
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +12,23 @@ import org.springframework.context.annotation.Profile;
 import static java.time.Duration.ofSeconds;
 
 @Configuration
-@Profile("openai")
-public class OpenAiConfiguration {
+public class AIConfiguration {
 
     @Bean
+    @Profile("openai")
     ChatLanguageModel chatModelOpenAi(DatabaseTool databaseTool) {
         return OpenAiChatModel.builder().apiKey("demo")
                 .modelName("gpt-4o-mini")
+                .timeout(ofSeconds(30)).temperature(0.0)
+                .build();
+    }
+
+    @Bean
+    @Profile("ollama")
+    ChatLanguageModel chatModelOllama(DatabaseTool databaseTool) {
+        return OllamaChatModel.builder()
+                .baseUrl("http://localhost:11434")
+                .modelName("llama3.1")
                 .timeout(ofSeconds(30)).temperature(0.0)
                 .build();
     }

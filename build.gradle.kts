48947b8a4fb642a4d8d3a0862773f4a8f4abe3e4
Andreas Mautsch <amautsch>
2024-11-02 10:09:39 +0100

    upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,17 @@ val version: String by project
 java.sourceCompatibility = JavaVersion.VERSION_21
 
 val dockerRegistry = "goafabric"
-val nativeBuilder = "dashaun/builder:20240403"
-val baseImage = "ibm-semeru-runtimes:open-21.0.1_12-jre-focal@sha256:24d43669156684f7bc28536b22537a7533ab100bf0a5a89702b987ebb53215be"
+val nativeBuilder = "paketobuildpacks/java-native-image:10.1.0"
+val baseImage = "ibm-semeru-runtimes:open-21.0.4.1_7-jre-focal@sha256:8b94f8b14fd1d4660f9dc777b1ad3630f847b8e3dc371203bcb857a5e74d6c39"
 
 plugins {
     java
     jacoco
-    id("org.springframework.boot") version "3.3.3"
+    id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
-    //id("org.graalvm.buildtools.native") version "0.10.2"
+    //id("org.graalvm.buildtools.native") version "0.10.3"
 
-    id("com.google.cloud.tools.jib") version "3.4.2"
+    id("com.google.cloud.tools.jib") version "3.4.4"
 }
 
 repositories {
@@ -22,15 +22,18 @@ repositories {
     maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
-val langchain4JVersion = "0.34.0"
 dependencies {
     constraints {
-        implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
-        implementation("org.mapstruct:mapstruct:1.5.5.Final")
-        annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-        implementation("io.github.resilience4j:resilience4j-spring-boot3:2.1.0")
+        annotationProcessor("org.mapstruct:mapstruct-processor:1.6.2")
+        implementation("org.mapstruct:mapstruct:1.6.2")
+        implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+        implementation("io.github.resilience4j:resilience4j-spring-boot3:2.2.0")
+        implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:1.0.5")
+        testImplementation("com.tngtech.archunit:archunit-junit5:1.3.0")
     }
 }
+
+val langchain4JVersion = "0.35.0"
 
 dependencies {
     //web

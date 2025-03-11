578529f5c4a08c6d241c7200685ef1b341aef65b
Andreas Mautsch <amautsch>
2025-03-11 15:34:13 +0100

    changes

--- src/main/java/org/goafabric/dbagentnew/AgentApplication.java
+++ src/main/java/org/goafabric/dbagentnew/AgentApplication.java
@@ -70,7 +70,16 @@ public class AgentApplication {
                         MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
                 hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.chat.ChatCompletionChoice$Builder"),
                         MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
-
+                hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.chat.Delta$Builder"),
+                        MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
+                hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.chat.Function$Builder"),
+                        MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
+                hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.chat.FunctionCall$Builder"),
+                        MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
+                hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.chat.Tool$Builder"),
+                        MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
+                hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.chat.ToolCall$Builder"),
+                        MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
             }

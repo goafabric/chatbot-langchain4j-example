aa13f2f6e573c35e212d1111906292d8313f33cd
Andreas Mautsch <amautsch>
2025-03-11 15:12:59 +0100

    changes

--- src/main/java/org/goafabric/dbagentnew/AgentApplication.java
+++ src/main/java/org/goafabric/dbagentnew/AgentApplication.java
@@ -2,17 +2,22 @@ package org.goafabric.dbagentnew;
 
 import org.goafabric.dbagentnew.ai.DatabaseAgent;
 import org.goafabric.dbagentnew.persistence.DemoDataImporter;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 import java.util.Scanner;
 
 
 @SpringBootApplication
+@ImportRuntimeHints(AgentApplication.ApplicationRuntimeHints.class)
 public class AgentApplication {
 
     public static void main(String[] args){
@@ -49,6 +54,28 @@ public class AgentApplication {
         };
     }
 
+
+    static class ApplicationRuntimeHints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            try {
+                hints.proxies().registerJdkProxy(DatabaseAgent.class);
+                hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.chat.ChatCompletionResponse$Builder"),
+                        MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
+                hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.shared.Usage$Builder"),
+                        MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
+                hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.shared.CompletionTokensDetails$Builder"),
+                        MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
+                hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.shared.PromptTokensDetails$Builder"),
+                        MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
+                hints.reflection().registerType(Class.forName("dev.ai4j.openai4j.chat.ChatCompletionChoice$Builder"),
+                        MemberCategory.DECLARED_CLASSES, MemberCategory.INTROSPECT_DECLARED_METHODS, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS);
+
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
 
 }

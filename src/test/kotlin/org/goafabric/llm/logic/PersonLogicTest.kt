7d900eed46e1993a7054594a8d2410e07090288a
Andreas Mautsch <amautsch>
2026-02-20 12:39:10 +0100

    upgrade

--- src/test/kotlin/org/goafabric/llm/logic/PersonLogicTest.kt
+++ src/test/kotlin/org/goafabric/llm/logic/PersonLogicTest.kt
@@ -5,50 +5,47 @@ import org.goafabric.llm.tool.logic.PersonLogic
 import org.goafabric.llm.tool.persistence.entity.Address
 import org.goafabric.llm.tool.persistence.entity.Allergy
 import org.goafabric.llm.tool.persistence.entity.Person
-import org.goafabric.llm.tool.persistence.entity.Person.address
-import org.goafabric.llm.tool.persistence.entity.Person.allergy
+
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-internal class PersonLogicTest {
-    @JvmField
-    @Autowired
-    var personLogic: PersonLogic? = null
+internal class PersonLogicTest(@Autowired val personLogic: PersonLogic) {
+
 
     @Test
     fun findAll() {
-        Assertions.assertThat<Person>(personLogic!!.findAll()).hasSize(4)
+        Assertions.assertThat<Person>(personLogic.findAll()).hasSize(4)
     }
 
 
     @Test
     fun findByFirstName() {
-        val person: MutableList<Person?> = personLogic!!.findByFirstName("homer")
+        val person: MutableList<Person> = personLogic.findByFirstName("homer")
         println(person)
         Assertions.assertThat<Person>(person).hasSize(1)
-        Assertions.assertThat<Address>(person.getFirst().address).hasSize(1)
-        Assertions.assertThat<Allergy>(person.getFirst().allergy).hasSize(1)
+        Assertions.assertThat<Address>(person.first().address).hasSize(1)
+        Assertions.assertThat<Allergy>(person.first().allergy).hasSize(1)
     }
 
     @Test
     fun findByLastName() {
-        val person: MutableList<Person?> = personLogic!!.findByLastName("sampson")
+        val person: MutableList<Person> = personLogic.findByLastName("sampson")
 
         println(person)
         Assertions.assertThat<Person>(person).hasSize(2)
-        Assertions.assertThat<Address>(person.getFirst().address).hasSize(1)
-        Assertions.assertThat<Allergy>(person.getFirst().allergy).hasSize(1)
+        Assertions.assertThat<Address>(person.first().address).hasSize(1)
+        Assertions.assertThat<Allergy>(person.first().allergy).hasSize(1)
     }
 
     @Test
     fun findByCity() {
-        val person: MutableList<Person?> = personLogic!!.findByCity("springfield")
+        val person: MutableList<Person> = personLogic.findByCity("springfield")
 
         println(person)
         Assertions.assertThat<Person>(person).hasSize(3)
-        Assertions.assertThat<Address>(person.getFirst().address).hasSize(1)
-        Assertions.assertThat<Allergy>(person.getFirst().allergy).hasSize(1)
+        Assertions.assertThat<Address>(person.first().address).hasSize(1)
+        Assertions.assertThat<Allergy>(person.first().allergy).hasSize(1)
     }
 }
